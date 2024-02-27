d44f1c2c99a678baba42778a524e6a0c41db0f8c
taharajati <125213375+taharajati@users.noreply.github.com>
2024-02-27 17:58:02 +0330

    007

--- src/components/mainTable/MyTable.jsx
+++ src/components/mainTable/MyTable.jsx
@@ -3,6 +3,7 @@ import DataFilter from './DataFilter';
 import MyChart from '../charts/MyChart';
 import Modal from './Modal';
 import SortableTableHeader from './SortableTableHeader';
+import CalculatorPopup from '../Calculator/CalculatorPopup';
 
 const MyTable = ({ filterValues }) => {
   const [intradata, setIntradata] = useState();
@@ -14,23 +15,21 @@ const MyTable = ({ filterValues }) => {
   const [validGroups, setValidGroups] = useState([]);
   const [showChart, setShowChart] = useState(false);
   const [chartData, setChartData] = useState(null);
-  const [hoveredRowIndex, setHoveredRowIndex] = useState(null)
-  const [selectedInstrumentId, setSelectedInstrumentId] = useState(null); // Add this line
+  const [hoveredRowIndex, setHoveredRowIndex] = useState(null);
+  const [selectedInstrumentId, setSelectedInstrumentId] = useState(null);
   const [sortCriteria, setSortCriteria] = useState('');
   const [sortOrder, setSortOrder] = useState('asc');
-
+  const [calculatorVisible, setCalculatorVisible] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Fetch intradatacols
         const intradatacolsResponse = await fetch('https://api.optionscreener.ir/api/options/intradatacols');
         if (!intradatacolsResponse.ok) {
           throw new Error(`HTTP error! Status: ${intradatacolsResponse.status}`);
         }
 
         const intradatacolsData = await intradatacolsResponse.json();
-        console.log('API Response for intradatacols:', intradatacolsData);
         setData(intradatacolsData);
 
         const groups = Object.keys(intradatacolsData.groups);
@@ -44,15 +43,12 @@ const MyTable = ({ filterValues }) => {
           return;
         }
 
-        // Fetch intradata
         const intradataResponse = await fetch('https://api.optionscreener.ir/api/options/intradata');
         if (!intradataResponse.ok) {
           throw new Error(`HTTP error! Status: ${intradataResponse.status}`);
         }
 
         const intradataData = await intradataResponse.json();
-        console.log('API Response for intradata:', intradataData);
-
         setIntradata(intradataData.data);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -68,8 +64,7 @@ const MyTable = ({ filterValues }) => {
   useEffect(() => {
     const fetchChartData = async () => {
       try {
-        // Fetch chart data from your API
-        const chartResponse = await fetch(`https://api.optionscreener.ir/api/options/chart?instrument_id=${selectedInstrumentId}`); // Replace 'YOUR_CHART_API_URL' with the actual API URL
+        const chartResponse = await fetch(`https://api.optionscreener.ir/api/options/chart?instrument_id=${selectedInstrumentId}`);
         if (!chartResponse.ok) {
           throw new Error(`HTTP error! Status: ${chartResponse.status}`);
         }
@@ -84,12 +79,11 @@ const MyTable = ({ filterValues }) => {
         });
       } catch (error) {
         console.error('Error fetching chart data:', error);
-        // Handle error accordingly
       }
     };
 
     fetchChartData();
-  }, [selectedInstrumentId]); // Empty dependency array to fetch data only once on component mount
+  }, [selectedInstrumentId]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -102,18 +96,17 @@ const MyTable = ({ filterValues }) => {
   if (!intradata || !data || !data.groups || !data.groupscolumn) {
     return <div>No data available</div>;
   }
-// Add a function to format numbers with a thousand separator
-const formatNumberWithSeparator = (number) => {
-  return number.toLocaleString();
-};
+
+  const formatNumberWithSeparator = (number) => {
+    return number.toLocaleString();
+  };
+
   const filteredData = DataFilter({
     intradata,
     filterValues,
     columns,
   });
 
-  
-  // Sorting function
   const sortData = (data, criteria, order) => {
     return data.sort((a, b) => {
       const valueA = typeof a[criteria] === 'number' ? a[criteria] : a[criteria]?.toLowerCase();
@@ -127,13 +120,10 @@ const formatNumberWithSeparator = (number) => {
     });
   };
 
-  // Handle sorting button click
   const handleSort = (criteria) => {
     if (sortCriteria === criteria) {
-      // Toggle sorting order if the same criteria is clicked again
       setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
     } else {
-      // Set new sorting criteria and default to ascending order
       setSortCriteria(criteria);
       setSortOrder('asc');
     }
@@ -143,36 +133,36 @@ const formatNumberWithSeparator = (number) => {
     <div className="container mt-4 mx-auto p-4" dir="rtl">
       <div className="flex items-center justify-between mb-3 ">
         <div className="space-x-1">
-        {validGroups.map((groupKey) => (
-  <button
-    key={groupKey}
-    type="button "
-    className={`btn ${selectedGroup === groupKey ? 'px-4 py-2 bg-[#2F657D] text-white rounded-lg scale-105 transition duration-500' : 'px-3 py-1 bg-[#F4F2F2] rounded-md'}`}
-    onClick={() => setSelectedGroup(groupKey)}
-  >
-    {data && data.groups && data.groups[groupKey] ? data.groups[groupKey] : 'Unknown Group'}
-  </button>
-))}
+          {validGroups.map((groupKey) => (
+            <button
+              key={groupKey}
+              type="button "
+              className={`btn ${selectedGroup === groupKey ? 'px-4 py-2 bg-[#2F657D] text-white rounded-lg scale-105 transition duration-500' : 'px-3 py-1 bg-[#F4F2F2] rounded-md'}`}
+              onClick={() => setSelectedGroup(groupKey)}
+            >
+              {data && data.groups && data.groups[groupKey] ? data.groups[groupKey] : 'Unknown Group'}
+            </button>
+          ))}
         </div>
       </div>
 
       <div className="m-2 items-center w-[1400px]">
         <div className="table-container overflow-x-auto overflow-y-auto" style={{ maxWidth: '2000px', maxHeight: '500px' }}>
           <table className="table-auto w-full border-collapse border border-gray-800" style={{ width: '100%' }}>
-          <thead className="bg-[#2F657D] text-white sticky top-0">
-  <tr>
-    {columns.map((column, index) => (
-      <SortableTableHeader
-        key={index}
-        column={{ fieldName: column, farsiName: data.fields[column] }}
-        criteria={sortCriteria}
-        order={sortOrder}
-        onSort={handleSort}
-      />
-    ))}
-    <th className="py-2 px-4 border border-[#343434] w-20"></th>
-  </tr>
-</thead>
+            <thead className="bg-[#2F657D] text-white sticky top-0">
+              <tr>
+                {columns.map((column, index) => (
+                  <SortableTableHeader
+                    key={index}
+                    column={{ fieldName: column, farsiName: data.fields[column] }}
+                    criteria={sortCriteria}
+                    order={sortOrder}
+                    onSort={handleSort}
+                  />
+                ))}
+                <th className="py-2 px-4 border border-[#343434] w-20"></th>
+              </tr>
+            </thead>
             <tbody>
               {filteredData.length > 0 ? (
                 sortData(filteredData, sortCriteria, sortOrder).map((item, itemIndex) => (
@@ -202,7 +192,18 @@ const formatNumberWithSeparator = (number) => {
                         >
                           📊
                         </span>
+                        
                       )}
+                        {/* Open Calculator Button */}
+      <div className="flex items-center space-x-2">
+        <button
+          className="px-4 py-2  text-white rounded-lg scale-105 transition duration-500"
+          onClick={() => setCalculatorVisible(true)}
+        >
+          💰
+        </button>
+      
+      </div>
                     </td>
                   </tr>
                 ))
@@ -218,6 +219,14 @@ const formatNumberWithSeparator = (number) => {
         </div>
       </div>
 
+    
+
+      {/* Calculator Popup */}
+      {calculatorVisible && (
+        <CalculatorPopup onClose={() => setCalculatorVisible(false)} />
+      )}
+
+      {/* Chart Modal */}
       {showChart && (
         <Modal onClose={() => setShowChart(false)}>
           {chartData ? (
@@ -235,6 +244,5 @@ const formatNumberWithSeparator = (number) => {
     </div>
   );
 };
-
 
 export default MyTable;

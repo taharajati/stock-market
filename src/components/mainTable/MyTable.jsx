b591600838dca57528e0db4039244502581902c7
ahmadrajati <ahmad.rajati995@gmail.com>
2024-08-18 16:30:50 +0430

    report

--- src/components/mainTable/MyTable.jsx
+++ src/components/mainTable/MyTable.jsx
@@ -285,9 +285,11 @@ import { IoMdCalculator } from "react-icons/io";
         } else if (columnName === 'bs_price_to_sell_price' && value < 0) {
             return { ...baseStyle, color: 'rgb(40,102,50)' ,backgroundColor:'rgb(163,240,173)' };
         }  else if (columnName === 'today_return' && value >=0) {
-            return { ...baseStyle, color: 'rgb(40,102,50)' ,backgroundColor:'rgb(163,240,173)' };
+            return { ...baseStyle, color: 'rgb(40,102,50)' //,backgroundColor:'rgb(163,240,173)'
+             };
         }  else if (columnName === 'today_return' && value < 0) {
-            return { ...baseStyle, color: 'rgb(102,43,50)' ,backgroundColor:'rgb(196,132,146)'};
+            return { ...baseStyle, color: 'rgb(102,43,50)' //,backgroundColor:'rgb(196,132,146)'
+            };
         }
 
 
